82ee1857e9509d4b5a65199dee4b13b4f14d502c
juanito16 <juan_alexander11@hotmail.com>
2014-03-19 21:13:11 -0600

    primer commic

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SolutionCr::Application.config.secret_key_base = '0b0ca689a644fb29c1ccb765767c1c0743f276729308fd4fb9643eb9c3ec74b68936e0cac802b3c521f7cc7369511590e70a0a225ddc20de2c6dd38c70c0f0f0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SolutionCr::Application.config.session_store :cookie_store, key: '_solution_cr_session'
