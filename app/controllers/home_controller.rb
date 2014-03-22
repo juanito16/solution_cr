f6d6b1ee1c17b17f91d7c9646b0326124bdad90f
juanito16 <juan_alexander11@hotmail.com>
2014-03-22 17:26:48 -0600

    add new vista

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
 	layout "home"
   def index
+  	@home="active"
+  end
+
+  def about
+  	@abaut="active"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 SolutionCr::Application.routes.draw do
   root "home#index"
+  get "home/about"=>"home#about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
